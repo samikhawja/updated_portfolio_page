3494e79fcbfa2c0626809686ea4cbc492ab778d0
samikhawja <skhawja11@gmail.com>
2021-10-10 23:28:26 -0700

    added modal for 'about me'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
